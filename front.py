0def3d8bd8bade576fc268b2452a4a8c51f73fbc
Ken Takasawa <ktakazaw@ncc.go.jp>
2023-06-30 12:09:53 +0900

    [Refactoring] コード成形 #3

--- front.py
+++ front.py
@@ -13,16 +13,32 @@ class Sample():
     
     def return_images(self):
         return [Image.open(f'data/{self.name}/{file}') for file in self.files]
-    
-
-
 
 def main():
-    st.set_page_config(layout='wide')
-    st.title('内視鏡 類似画像供覧ツール')
 
+    # ========= Define functions =========
+    def push_tbl():
+        df_var = pd.DataFrame({'Query name': [sample.name for _ in range(len(sample.suggestion) - 1)],
+                               'Suggested file': sample.suggestion[1:],
+                               'Person 1': [v for k, v in st.session_state.items() if 'p1' in k],
+                               'Person 2': [v for k, v in st.session_state.items() if 'p2' in k],
+                               'Person 3': [v for k, v in st.session_state.items() if 'p3' in k]})
+        st.session_state.df = pd.concat([df_var, st.session_state.df])
+        st.session_state.log[st.session_state.counter] = 1
+
+    def minus_counter():
+        if st.session_state:
+            st.session_state['counter'] -= 1
+        
+
+    def plus_counter():
+        st.session_state['counter'] += 1
+
+
+    # ========= Initializing app state =========
     if 'samples' not in st.session_state: 
         st.session_state.samples = [path for path in os.listdir('data') if os.path.isdir(os.path.join('data', path))]
+        st.session_state.log = [0 for _ in range(len(st.session_state.samples))]
         st.session_state.counter = 0
         st.session_state.df = pd.DataFrame({'Query name': [], 
                                             'Suggested file': [],
@@ -32,14 +48,29 @@ def main():
 
     sample = Sample(st.session_state.samples[st.session_state.counter])
     pictures = sample.return_images()
-    st.write(f'Query file is: {sample.name}')
 
+
+    # ======== Viewer =========
+    ## >>>> Page setting <<<<<
+    st.set_page_config(layout='wide')
+
+    ## >>>> Ttitle <<<<
+    st.title('内視鏡 類似画像供覧ツール')
+
+    ## >>>> Query name <<<<
+    if st.session_state.log[st.session_state.counter]:
+        st.markdown(f'Query file is: {sample.name} **:red[供覧済みです!]**')
+    else:
+        st.markdown(f'Query file is: {sample.name}')
+
+    ## >>>> Pictures with tile-like viewing <<<<
     for row in range(-(-len(pictures)) // 3):
         for i, col in enumerate(st.columns(3)):
             k = row * 3 + i
             col.subheader(sample.suggestion[k])
             col.image(pictures[k])
 
+    ## >>>> Input form <<<<
     with st.form('供覧結果', clear_on_submit=True):
         c1, c2, c3, c4 = st.columns(4)
         with c1:
@@ -50,37 +81,24 @@ def main():
             c2.subheader('Preson 2')
             for i in range(1, len(pictures)):
                 c2.radio(sample.suggestion[i], ("OK", 'Bad'), key = f'p2{i}', horizontal=True, args=[1, 0])
-
         with c3:
             c3.subheader('Preson 3')
             for i in range(1, len(pictures)):
                 c3.radio(sample.suggestion[i], ("OK", 'Bad'), key = f'p3{i}', horizontal=True, args=[1, 0])
-
         with c4:
-            push = st.form_submit_button(label = '供覧結果を転機')
-    
-    def minus_counter():
-        st.session_state['counter'] -= 1
+            st.form_submit_button(label = '供覧結果を転機', on_click = push_tbl)
 
-    def plus_counter():
-        st.session_state['counter'] += 1
-
+    ## >>>> Move next and previous buttons <<<<
     col1, col2 =st.columns(2)
     with col1:
         st.button('<< 前の画像へ', on_click=minus_counter)
     with col2:
         st.button('次の画像へ >>', on_click=plus_counter)
 
-    if push:
-        df_var = pd.DataFrame({'Query name': [sample.name for _ in range(len(sample.suggestion) - 1)],
-                               'Suggested file': sample.suggestion[1:],
-                               'Person 1': [v for k, v in st.session_state.items() if 'p1' in k],
-                               'Person 2': [v for k, v in st.session_state.items() if 'p2' in k],
-                               'Person 3': [v for k, v in st.session_state.items() if 'p3' in k]})
-        st.session_state.df = pd.concat([df_var, st.session_state.df])
-
+    ## >>>> Data table of reviewing results <<<<
     edited_df = st.data_editor(st.session_state.df, num_rows='dynamic')
 
+    ## >>>> Download button <<<<
     st.download_button(label = 'Download csv file',
                        data = edited_df.to_csv().encode('shift-jis'),
                        mime = 'text/csv',
